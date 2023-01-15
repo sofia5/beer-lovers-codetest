8da400f5b65f64392061421022481be671ffd2a5
Sofia Johansson <sofia.johansson@propstreet.com>
2023-01-15 23:10:11 +0100

    Modify filter

--- src/components/beerList.tsx
+++ src/components/beerList.tsx
@@ -2,19 +2,29 @@ import useBeers from "../hooks/useBeers";
 import BeerItem from "./beerItem";
 import LoadingSpinner from "./loadingSpinner";
 import { Table, Card } from "react-bootstrap";
-import FilterRange from "./filterRange";
 import SearchBar from "./searchBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import MultiRangeSlider from "./multiRangeSlider";
+import styles from "../scss/beerList.module.scss";
+import { Beer } from "../types/interfaces";
 
 const BeerList = () => {
   const [filterOpen, setFilterOpen] = useState(false);
+  const [filteredBeerList, setFilteredBeerList] = useState<Beer[]>([]);
+  const [abvsFilter, setAbvsFilter] = useState<Beer[]>([]);
   const { beers, loading, error } = useBeers();
 
   beers.sort((a, b) => {
     return b.abv - a.abv;
   });
+
+  // Initialize abvsFilter to the original list of beers
+  useEffect(() => {
+    setFilteredBeerList(beers);
+    setAbvsFilter(beers);
+  }, [beers]);
 
   if (loading) {
     return <LoadingSpinner />;
@@ -23,7 +33,22 @@ const BeerList = () => {
     return <div>{error}</div>;
   }
 
+  const setNewAbvsFilter = (min: number, max: number) => {
+    console.log(`min = ${min}, max = ${max}`);
+    // let p2 = Object.assign({}, beers);
+    // console.log(p2);
+    // setFilteredBeerList(p2.filter((b) => min <= b.abv && b.abv <= max));
+
+    // setAbvsFilter(
+    //   Math.min(...abvs) < min || max < Math.max(...abvs)
+    //     ? p2.filter((b) => min <= b.abv && b.abv <= max)
+    //     : p2
+    // );
+  };
+
   const abvs = beers.map((b) => b.abv);
+
+  // const filteredBeerList = abvsFilter;
 
   return (
     <>
@@ -33,7 +58,7 @@ const BeerList = () => {
             <div className="row d-flex justify-content-between align-items-baseline">
               <div className="col-1 filter-wrapper">
                 <FontAwesomeIcon
-                  className={`fa-xl filter-icon ${
+                  className={`fa-xl ${styles["filter-icon"]} ${
                     filterOpen ? "text-success" : ""
                   }`}
                   icon={faFilter}
@@ -44,20 +69,24 @@ const BeerList = () => {
                 <SearchBar items={beers}></SearchBar>
               </div>
             </div>
-            {filterOpen}
             {filterOpen && (
               <div className="row mt-3">
                 <h4 className="text-success">Filters</h4>
-                <FilterRange
-                  range={abvs}
+                <MultiRangeSlider
+                  min={Math.min(...abvs)}
+                  max={Math.max(...abvs)}
+                  onChange={
+                    // ({ min, max }) => console.log(`min = ${min}, max = ${max}`)
+                    ({ min, max }) => setNewAbvsFilter(min, max)
+                  }
                   label="Alcohol by volume"
-                ></FilterRange>
+                />
               </div>
             )}
             <div className="row mt-5">
               <div className="w-100">
                 <Table hover variant="dark" className="mb-0 p-3">
-                  <thead className="table-head">
+                  <thead className={styles["table-head"]}>
                     <tr className="text-uppercase text-success">
                       <th>Name</th>
                       <th>Tagline</th>
@@ -65,9 +94,13 @@ const BeerList = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {beers.map((b) => (
-                      <BeerItem key={b.id} beer={b} />
-                    ))}
+                    {filteredBeerList.length > 0 &&
+                      filteredBeerList.map((b) => (
+                        <BeerItem key={b.id} beer={b} />
+                      ))}
+                    {/* {filteredBeerList.length === 0 && (
+                      <p>No beers available with this filter</p>
+                    )} */}
                   </tbody>
                 </Table>
               </div>
---
+++ src/components/multiRangeSlider.tsx
@@ -0,0 +1,106 @@
+import { useCallback, useEffect, useState, useRef } from "react";
+import PropTypes from "prop-types";
+import styles from "../scss/multiRangeSlider.module.scss";
+
+const MultiRangeSlider = ({
+  min,
+  max,
+  onChange,
+  label,
+}: {
+  min: number;
+  max: number;
+  onChange: (range: { min: number; max: number }) => void;
+  label: string;
+}) => {
+  const [minVal, setMinVal] = useState(min);
+  const [maxVal, setMaxVal] = useState(max);
+  const minValRef = useRef(min);
+  const maxValRef = useRef(max);
+  const range = useRef(null);
+
+  // Convert to percentage
+  const getPercent = useCallback(
+    (value: number) => Math.round(((value - min) / (max - min)) * 100),
+    [min, max]
+  );
+
+  // Set width of the range to decrease from the left side
+  useEffect(() => {
+    const minPercent = getPercent(minVal);
+    const maxPercent = getPercent(maxValRef.current);
+
+    if (range.current) {
+      (range.current as HTMLElement).style.left = `${minPercent}%`;
+      (range.current as HTMLElement).style.width = `${
+        maxPercent - minPercent
+      }%`;
+    }
+  }, [minVal, getPercent]);
+
+  // Set width of the range to decrease from the right side
+  useEffect(() => {
+    const minPercent = getPercent(minValRef.current);
+    const maxPercent = getPercent(maxVal);
+
+    if (range.current) {
+      (range.current as HTMLElement).style.width = `${
+        maxPercent - minPercent
+      }%`;
+    }
+  }, [maxVal, getPercent]);
+
+  // Get min and max values when their state changes
+  useEffect(() => {
+    onChange({ min: minVal, max: maxVal });
+  }, [minVal, maxVal, onChange]);
+
+  return (
+    <div className="container">
+      <label className="text-uppercase text-white mt-4 mb-4">{label}</label>
+      <div>
+        <input
+          type="range"
+          min={min}
+          max={max}
+          value={minVal}
+          onChange={(event) => {
+            const value = Math.min(Number(event.target.value), maxVal - 1);
+            setMinVal(value);
+            minValRef.current = value;
+          }}
+          className={`${styles.thumb} ${styles["thumb--left"]}`}
+          // style={{ zIndex: minVal > max - 100 && "5" }}
+        />
+
+        <input
+          type="range"
+          min={min}
+          max={max}
+          value={maxVal}
+          onChange={(event) => {
+            const value = Math.max(Number(event.target.value), minVal + 1);
+            setMaxVal(value);
+            maxValRef.current = value;
+          }}
+          className={`${styles.thumb} ${styles["thumb--right"]}`}
+        />
+
+        <div className={styles["slider"]}>
+          <div className={styles["slider-track"]} />
+          <div ref={range} className={styles["slider-range"]} />
+          <div className={styles["slider-left-value"]}>{minVal}</div>
+          <div className={styles["slider-right-value"]}>{maxVal}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+MultiRangeSlider.propTypes = {
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default MultiRangeSlider;
